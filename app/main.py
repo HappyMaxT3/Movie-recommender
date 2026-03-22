b236a53d9d17e76f136ca505e7ee1c837a4fccd1
Max <m9775531181@gmail.com>
2026-03-23 00:15:21 +0300

    configured api

--- app/main.py
+++ app/main.py
@@ -1,1 +1,39 @@
-print("hello wrld")
+from app.omdb_api import search_movies, get_movie_details
+
+
+def main():
+    print("=== Movie Search CLI ===")
+
+    while True:
+        query = input("\nEnter movie name (or '0' to exit): ")
+
+        if query.lower() == "0":
+            break
+
+        results = search_movies(query)
+
+        if not results:
+            print("No movies found")
+            continue
+
+        for i, movie in enumerate(results):
+            print(f"{i+1}. {movie['Title']} ({movie['Year']})")
+
+        try:
+            choice = int(input("Select movie number: ")) - 1
+            selected = results[choice]
+        except:
+            print("Invalid choice")
+            continue
+
+        movie_details = get_movie_details(selected["imdbID"])
+
+        print("\n=== Movie Details ===")
+        print(f"Title: {movie_details['title']}")
+        print(f"Year: {movie_details['year']}")
+        print(f"Genre: {movie_details['genre']}")
+        print(f"Plot: {movie_details['overview']}")
+
+
+if __name__ == "__main__":
+    main()
--- app/omdb_api.py
+++ app/omdb_api.py
@@ -1,3 +1,40 @@
-from app.config import OMDB_API_KEY
+import requests
+from app.config import OMDB_API_KEY, BASE_URL
 
-print(OMDB_API_KEY)
+
+def search_movies(title):
+    params = {
+        "apikey": OMDB_API_KEY,
+        "s": title
+    }
+
+    response = requests.get(BASE_URL, params=params)
+    data = response.json()
+    print(data)
+
+    if data.get("Response") == "False":
+        return []
+
+    return data.get("Search", [])
+
+
+def get_movie_details(imdb_id):
+    params = {
+        "apikey": OMDB_API_KEY,
+        "i": imdb_id,
+        "plot": "full"
+    }
+
+    response = requests.get(BASE_URL, params=params)
+    data = response.json()
+
+    if data.get("Response") == "False":
+        return None
+
+    return {
+        "id": imdb_id,
+        "title": data.get("Title"),
+        "year": data.get("Year"),
+        "overview": data.get("Plot"),
+        "genre": data.get("Genre")
+    }
